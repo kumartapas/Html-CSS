ef894f3e9438427227cd85e95ba98b6bef2e5ec0
kumartapas <115998636+kumartapas@users.noreply.github.com>
2022-10-31 18:21:50 +0530

    Add files via upload

---
+++ seri.js
@@ -0,0 +1,12 @@
+
+    function seri()
+{
+   const n= prompt("enter the number:");
+  console.log("the number is"+n);
+   let n1;
+ for(n1=2;n1<=20;n1+=2)
+ {
+     console.log(n1*n1);
+  }
+     }
+ 
